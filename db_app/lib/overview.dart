0119362b10fe0687e724778399f44f5ccf9f3c67
Timo Angerer <taimonania@yahoo.de>
2020-11-11 07:38:06 +0100

    make the bottom navigation bar interactive

--- db_app/lib/overview.dart
+++ db_app/lib/overview.dart
@@ -9,6 +9,19 @@ class Overview extends StatefulWidget {
 }
 
 class _OverviewState extends State<Overview> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _selectedPage = <Widget>[
+    Text('Overview Screen: list of meals goes here'),
+    Text('Edit Screen: see and edit one meal here'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,19 +30,22 @@ class _OverviewState extends State<Overview> {
       ),
       body: Container(
         alignment: Alignment.center,
-        child: Text('List of meals goes here'),
+        child: _selectedPage.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: "home",
+            label: "Home",
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.edit),
-            label: "edit",
+            label: "Edit",
           ),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
