b8a2b1ddb13bd29c69001d9b3502d31122fc22ab
Timo Angerer <taimonania@yahoo.de>
2020-11-13 10:00:00 +0100

    WIP on storing files locally

---
+++ db_app/lib/data_helper.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:localstorage/localstorage.dart';
+
+class MealItem {
+  String name;
+  bool tasty;
+
+  MealItem({this.name, this.tasty});
+
+  toJSONEncodable() {
+    Map<String, dynamic> m = new Map();
+
+    m['name'] = name;
+    m['tasty'] = tasty;
+
+    return m;
+  }
+}
+
+class MealList {
+  List<MealItem> meals;
+
+  MealList(List<MealItem> mealList) {
+    meals = new List();
+  }
+
+  MealList.nonempty(List<MealItem> mealList) {
+    meals = mealList;
+  }
+
+  void add(MealItem item) {
+    meals.add(item);
+  }
+
+  toJSONEncodable() {
+    return meals.map((meal) {
+      return meal.toJSONEncodable();
+    }).toList();
+  }
+}
+
+class DataService {
+  LocalStorage storage = new LocalStorage('meals');
+
+  void storeMeals(MealList meals) async {
+    await storage.ready;
+    storage.setItem('meals', meals);
+  }
+
+  Future<MealList> getMeals() async {
+    await storage.ready;
+    return storage.getItem('meals');
+  }
+
+  void clearMeals() async {
+    await storage.ready;
+    storage.clear();
+  }
+}
--- db_app/lib/overview.dart
+++ db_app/lib/overview.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'data_helper.dart';
 
 class Overview extends StatefulWidget {
   Overview({Key key, this.title}) : super(key: key);
@@ -10,16 +13,22 @@ class Overview extends StatefulWidget {
 
 class _OverviewState extends State<Overview> {
   int _selectedIndex = 0;
-  List<String> _meals = <String>[
-    "Dakgalbi",
-    "Korean BBQ",
-    "Jjimdak",
-    "Bibimbap",
-    "Gimbap",
-    "Ttoppokki",
-    "Cold Noodles",
-    "Rice Cake",
-  ];
+  MealList _meals;
+  DataService service = new DataService();
+  TextEditingController controller = new TextEditingController();
+
+  String currentEdit = "";
+
+  _OverviewState() {
+    List<MealItem> _mealList = <MealItem>[
+      MealItem(name: "Dakgalbi", tasty: false),
+      MealItem(name: "Korean BBQ", tasty: false),
+      MealItem(name: "Jjimdak", tasty: false),
+      MealItem(name: "Bibimbap", tasty: false),
+      MealItem(name: "Gimbap", tasty: false),
+    ];
+    _meals = new MealList.nonempty(_mealList);
+  }
 
   Widget _selectedPage() {
     switch (_selectedIndex) {
@@ -35,24 +44,70 @@ class _OverviewState extends State<Overview> {
     }
   }
 
+  void _save() {
+    _meals.add(MealItem(name: controller.value.text, tasty: false));
+    print("A new meal was saved: " + controller.value.text);
+    controller.clear();
+  }
+
   Widget _buildEditPage() {
-    return Text('Edit Screen: see and edit one meal here');
+    controller.
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: 'Want to add a new meal?',
+            ),
+            onEditingComplete: _save,
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.save),
+                onPressed: _save,
+                tooltip: 'Save',
+              ),
+              IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: service.clearMeals,
+                tooltip: 'Clear storage',
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
   Widget _buildMealsPage() {
     return ListView.builder(
         padding: EdgeInsets.all(16.0),
-        itemCount: _meals.length * 2, // length * 2 because of the dividers
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-          final index = i ~/ 2;
-          return _buildMealRow(_meals[index]);
+        itemCount:
+            _meals.meals.length * 2, // length * 2 because of the dividers
+        itemBuilder: (BuildContext context, int i) {
+          return GestureDetector(
+            child: (() {
+              if (i.isOdd) return Divider();
+              final index = i ~/ 2;
+              return _buildMealRow(_meals.meals[index]);
+            }()),
+            onTap: () => Scaffold.of(context)
+                .showSnackBar(SnackBar(content: Text(i.toString()))),
+          );
         });
+    // itemBuilder: (BuildContext context, int i) {
+    //   if (i.isOdd) return Divider();
+    //   final index = i ~/ 2;
+    //   return _buildMealRow(_meals.meals[index]);
+    // });
   }
 
-  Widget _buildMealRow(String meal) {
+  Widget _buildMealRow(MealItem meal) {
     return ListTile(
-      title: Text(meal),
+      title: Text(meal.name),
     );
   }
 
