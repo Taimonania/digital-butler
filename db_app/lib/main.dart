10ea918556d2f912ae1bed9fddc2c540b9017405
Timo Angerer <taimonania@yahoo.de>
2020-11-19 08:47:40 +0100

    make overview and editpage to seperate widgets
    The data storage isn't fully working anymore

--- db_app/lib/main.dart
+++ db_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'editpage.dart';
 import 'overview.dart';
 
 void main() {
@@ -15,7 +16,73 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: Overview(title: 'Digital Butler'),
+      home: MainView(title: 'Digital Butler'),
+    );
+  }
+}
+
+class MainView extends StatefulWidget {
+  MainView({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MainViewState createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  // index for the currently selected bottom menu
+  int _selectedIndex = 0;
+  Overview overview;
+
+  _MainViewState() {
+    overview = Overview();
+  }
+
+  Widget _selectedPage() {
+    switch (_selectedIndex) {
+      case 0:
+        return Overview(title: widget.title);
+        break;
+      case 1:
+        return EditPage(title: widget.title);
+        break;
+      default:
+        return Text(
+            "Error: Default case for selected index. This should not happen.");
+    }
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: _selectedPage(), //_selectedPage.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.edit),
+            label: "Edit",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
--- db_app/lib/overview.dart
+++ db_app/lib/overview.dart
@@ -12,9 +12,6 @@ class Overview extends StatefulWidget {
 }
 
 class _OverviewState extends State<Overview> {
-  // index for the currently selected bottom menu
-  int _selectedIndex = 0;
-
   MealList _meals;
   DataService service = new DataService();
   TextEditingController controller = new TextEditingController();
@@ -24,65 +21,21 @@ class _OverviewState extends State<Overview> {
   _OverviewState() {
     _meals = MealList();
 
-    // _meals.add(MealItem(name: "Dakgalbi", tasty: false));
-    // _meals.add(MealItem(name: "Korean BBQ", tasty: false));
-    // _meals.add(MealItem(name: "Jjimdak", tasty: false));
-    // _meals.add(MealItem(name: "Bibimbap", tasty: false));
-    // _meals.add(MealItem(name: "Gimbap", tasty: false));
+    _meals.add(MealItem(name: "Dakgalbi", tasty: false));
+    _meals.add(MealItem(name: "Korean BBQ", tasty: false));
+    _meals.add(MealItem(name: "Jjimdak", tasty: false));
+    _meals.add(MealItem(name: "Bibimbap", tasty: false));
+    _meals.add(MealItem(name: "Gimbap", tasty: false));
   }
 
-  Widget _selectedPage() {
-    switch (_selectedIndex) {
-      case 0:
-        return _buildMealsPage();
-        break;
-      case 1:
-        return _buildEditPage();
-        break;
-      default:
-        return Text(
-            "Error: Default case for selected index. This should not happen.");
-    }
-  }
-
-  void _save() {
-    _meals.add(MealItem(name: controller.value.text, tasty: false));
-    print("A new meal was saved: " + controller.value.text);
-    controller.clear();
-  }
-
-  Widget _buildEditPage() {
-    return Column(
-      children: <Widget>[
-        ListTile(
-          title: TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              labelText: 'Want to add a new meal?',
-            ),
-            onEditingComplete: _save,
-          ),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.save),
-                onPressed: _save,
-                tooltip: 'Save',
-              ),
-              IconButton(
-                icon: Icon(Icons.delete),
-                onPressed: service.clearMeals,
-                tooltip: 'Clear storage',
-              )
-            ],
-          ),
-        ),
-      ],
+  Widget _buildMealRow(MealItem meal) {
+    return ListTile(
+      title: Text(meal.name),
     );
   }
 
-  Widget _buildMealsPage() {
+  @override
+  Widget build(BuildContext context) {
     return FutureBuilder<MealList>(
         future: service.getMeals(),
         builder: (BuildContext context, AsyncSnapshot<MealList> snapshot) {
@@ -105,45 +58,5 @@ class _OverviewState extends State<Overview> {
                 );
               });
         });
-  }
-
-  Widget _buildMealRow(MealItem meal) {
-    return ListTile(
-      title: Text(meal.name),
-    );
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: _selectedPage(), //_selectedPage.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.edit),
-            label: "Edit",
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
   }
 }
