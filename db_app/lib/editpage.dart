553b383a58dbfda93ed0f744163992c03f84b66e
cade lueker <cadelueker@cades-MacBook-Pro.local>
2020-11-25 16:55:44 +0900

    yes

--- db_app/lib/editpage.dart
+++ db_app/lib/editpage.dart
@@ -1,11 +1,13 @@
+import 'dart:html';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'data_helper.dart';
 import 'package:image_picker/image_picker.dart';
 
 class EditPage extends StatefulWidget {
-  EditPage({Key key, this.title}) : super(key: key);
+  EditPage({Key key, this.title, this.description}) : super(key: key);
   final String title;
+  final String description;
 
   @override
   _EditPageState createState() => _EditPageState();
@@ -32,7 +34,8 @@ class _EditPageState extends State<EditPage> {
   }
 
   void _save() {
-    service.addMeal(MealItem(name: controller.value.text, tasty: false));
+    service.addMeal(MealItem(
+        name: controller.value.text, description: controller.value.text));
     print("A new meal was saved: " + controller.value.text);
     controller.clear();
   }
@@ -61,11 +64,7 @@ class _EditPageState extends State<EditPage> {
                 icon: Icon(Icons.camera),
                 onPressed: getImage,
               ),
-              Center(
-                  //checking if the image is null
-                  child: _image == null
-                      ? Text("image is not loaded")
-                      : Image.file(_image)),
+
               // this icon when pressed clears all of the meals saved which
               // is not desired
               // IconButton(
@@ -76,6 +75,20 @@ class _EditPageState extends State<EditPage> {
             ],
           ),
         ),
+        Container(
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: 'How was the meal?',
+            ),
+            onEditingComplete: _save,
+          ),
+        ),
+        Center(
+            //checking if the image is null
+            child: _image == null
+                ? Text("image is not loaded")
+                : Image.file(_image)),
       ],
     );
   }
